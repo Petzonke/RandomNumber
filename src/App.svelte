4a3a43add92b4dcdfde00bcc24bd6f3d9a881a33
Phethani Mudau <phethani@mushegroup.com>
2023-10-27 10:28:53 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import RandomNumber from './RandomNumber.svelte';
+  </script>
+  
+  <main>
+	<RandomNumber />
+  </main>
+  
+  <style>
+	main {
+	  text-align: center;
+	  padding: 1em;
+	  max-width: 240px;
+	  margin: 0 auto;
+	}
+  </style>
+  
---
+++ src/RandomNumber.svelte
@@ -0,0 +1,13 @@
+<script>
+  let randomNumber = 0;
+
+  function generateRandomNumber() {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+  }
+</script>
+
+<div>
+  <h2>Random Number Generator</h2>
+  <button on:click={generateRandomNumber}>Generate Random Number</button>
+  <p>Random Number: {randomNumber}</p>
+</div>
